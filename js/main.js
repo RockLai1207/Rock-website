9fcc11c4bd4bc09e54824d3892038c065777b082
Rock Lai <jclai1207@gmail.com>
2016-08-12 17:05:18 +0800

    Fix Error

--- js/main.js
+++ js/main.js
@@ -3,22 +3,6 @@ jQuery(document).ready(function($) {
 	$('.cd-main-nav').on('click', function(event) {
 		if ($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
 	});
-  		// url: "https://www.instagram.com/oauth/authorize/?client_id=d5893d8fdc884db6b149359aca01b320&redirect_uri=https://rocklai1207.github.io/Rock-website/&response_type=code",
-  		// url: "https://www.instagram.com/oauth/authorize/?client_id=95e6922266da418a88cae604222b7d76&redirect_uri=http://localhost&response_type=code",
-	$.ajax({
-	  		url: "https://www.instagram.com/oauth/authorize/?client_id=d5893d8fdc884db6b149359aca01b320&redirect_uri=https://rocklai1207.github.io/Rock-website/&response_type=code",
-	        beforeSend: function(xhr) {
-	             xhr.setRequestHeader("Access-Control-Allow-Origin", "*")
-	        }, success: function(data){
-		console.log(data);
-		 if (data.redirect) {
-	            // data.redirect contains the string URL to redirect to
-	            window.location.href = data.redirect;
-	        }
-	 //     alert(JSON.stringify(data));
-	            //process the JSON data etc
-	        }
-	})
 
 	// $.ajax({
 	// 	type: "GET",
